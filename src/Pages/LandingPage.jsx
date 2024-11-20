70328d4aac86f994da17d6160ca6c8913125a2a8
ufik0203@gmail.com <ufik0203@gmail.com>
2024-11-20 14:13:53 +0700

    Update RM

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,7 +5,8 @@ const LandingPage = () => {
         <div className="min-h-screen">
             <Navbar></Navbar>
             <div className="h-full w-full fixed flex justify-center items-center">
-                <div className="bg-[url('./images/roadmap.png')] min-h-[50rem] w-full mx-20 rounded-xl flex justify-center items-center mb-36 bg-cover bg-center bg-no-repeat">
+                <div className="min-h-[50rem] w-full md:mx-20 rounded-xl flex justify-center items-center mb-36">
+                    <img src="./images/roadmap.png" alt="" className="md:h-[50rem] h-[20rem] w-[32rem] md:w-full md:mx-20 rounded-xl bg-cover bg-center bg-no-repeat" />
                 </div>
             </div>
         </div>
