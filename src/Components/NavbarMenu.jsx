825c515089101cc35f504d56d0afe34bbce44977
ufik0203@gmail.com <ufik0203@gmail.com>
2024-11-22 09:44:16 +0700

    Update State

--- src/Components/NavbarMenu.jsx
+++ src/Components/NavbarMenu.jsx
@@ -2,13 +2,13 @@ import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 
 const NavbarMenu = (props) => {
-    const { children, path='#', clsname } =props;
+    const { children, path='#', clsname='hover:text-orange-500' } =props;
     return (
         <motion.div
             initial={{ opacity: 1, scale: 1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 15 }}
-            className={`select-none ${clsname} h-full flex justify-center items-center text-center hover:text-orange-500 hover:cursor-pointer`}
+            className={`select-none ${clsname} h-full flex justify-center items-center text-center hover:cursor-pointer`}
         >
             <Link to={`${path}`} className={`text-[11px] sm:text-sm ${clsname}`}>{children}</Link>
         </motion.div>
